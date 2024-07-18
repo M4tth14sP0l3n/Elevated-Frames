5ea2ea91282bac21bab38d652e601a05d706af2e
M4tth142 <matthias.polen@gmail.com>
2024-07-18 20:46:45 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,21 @@ import '../styles/About.css';
 const About = () => {
   return (
     <div className="about">
-      <h2>About Me</h2>
-      <p>Hello! My name is Matthias Polen, a 22-year-old software developer with a passion for programming and technology. 
-        I recently graduated from Hogent University, where I earned a degree in programming.
-        My primary expertise lies in C# and .NET, including extensive experience with Entity Framework. I am also proficient in front-end technologies such as React and React Native, as well as JavaScript, HTML, and CSS. Additionally, I have a strong foundation in database management, utilizing MySQL, MongoDB, and Google Firestore.
-        Beyond my technical skills, I am also an avid drone pilot and amateur photographer, hobbies that allow me to explore my creativity and attention to detail.
-        I believe in the importance of lifelong learning and staying updated with the latest advancements in technology. My goal is to continuously improve my skills and contribute to innovative projects. I am driven by the challenge of solving complex problems and finding efficient solutions. My aim is to leverage my programming skills to create impactful software that addresses real-world issues.
-</p>
-    </div>
+  <h2>About Me</h2>
+  <p>
+    Hello! My name is Matthias Polen, a 22-year-old software developer with a passion for programming and technology. I recently graduated from Hogent University, where I earned a degree in programming.
+  </p>
+  <p>
+    My primary expertise lies in C# and .NET, including extensive experience with Entity Framework. I am also proficient in front-end technologies such as React and React Native, as well as JavaScript, HTML, and CSS. Additionally, I have a strong foundation in database management, utilizing MySQL, MongoDB, and Google Firestore.
+  </p>
+  <p>
+    Beyond my technical skills, I am also an avid drone pilot and amateur photographer, hobbies that allow me to explore my creativity and attention to detail.
+  </p>
+  <p>
+    I believe in the importance of lifelong learning and staying updated with the latest advancements in technology. My goal is to continuously improve my skills and contribute to innovative projects. I am driven by the challenge of solving complex problems and finding efficient solutions. My aim is to leverage my programming skills to create impactful software that addresses real-world issues.
+  </p>
+</div>
+
   );
 };
 

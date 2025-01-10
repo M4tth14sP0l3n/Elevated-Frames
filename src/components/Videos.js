0ff977e6693b980eecd504ebc1d3c19b2acf65e5
M4tth142 <matthias.polen@gmail.com>
2025-01-10 21:21:49 +0100

    final fixes

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -2,22 +2,27 @@ import React from "react";
 import "../styles/Videos.css";
 
 import Video1 from "../videocollection/Final Web.mp4";
-import thumbnail1 from "../videocollection/Timeline 1_01_00_09_19.jpg";
+import thumbnail1 from "../videocollection/kouterclinic-thumbnail.jpg";
 import Video2 from "../videocollection/fall run.mp4";
-import thumbnail2 from "../videocollection/Timeline 1_01_00_09_19.jpg";
+import thumbnail2 from "../videocollection/seba reel thumbnail.jpg";
 import Video3 from "../videocollection/final .mp4";
-import thumbnail3 from "../videocollection/Timeline 1_01_00_09_19.jpg";
+import thumbnail3 from "../videocollection/autumn thumbnail.jpg";
 import Video4 from "../videocollection/arm day.mp4";
-import thumbnail4 from "../videocollection/Timeline 1_01_00_09_19.jpg";
+import thumbnail4 from "../videocollection/jaimin thumbnail.jpg";
+import Video5 from "../videocollection/Promo Me V1.mp4";
+import thumbnail5 from "../videocollection/promo vid me .jpg";
 
 const Videos = () => {
   return (
     <section id="videos" className="videos">
-      <h2>Videos</h2>
+      <h2>Promotional videos</h2>
       <div className="video-gallery-wide">        
         <video src={Video1}  poster={thumbnail1} controls></video>
+        <video src={Video5}  poster={thumbnail5} controls></video>
+
+
       </div>
-      <h2></h2>
+      <h2>Recreational videos</h2>
 
       <div className="video-gallery">
         <video src={Video4} poster={thumbnail2} controls></video>

ba3d4bba0184809a7a0d89fee8c516185a6b64f4
M4tth142 <matthias.polen@gmail.com>
2025-02-28 21:38:33 +0100

    additions

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -13,13 +13,13 @@ const Pricing = () => {
         </p>
         <div>
           <p>
-            <strong>Basis:</strong> 4 uur, 200–300 foto’s, basisbewerking, 1 locatie - €325
+            <strong>Basis:</strong> 4 uur, 200–300 foto’s, basisbewerking, 1 locatie - €199
           </p>
           <p>
-            <strong>Standaard:</strong> 8 uur, 400–600 foto’s, geavanceerde bewerking, 2 locaties - €600
+            <strong>Standaard:</strong> 6 uur, 300–500 foto’s, geavanceerde bewerking, 2 locaties - €350
           </p>
           <p>
-            <strong>Premium:</strong> 10–12 uur, 600+ foto’s, volledige evenement, geavanceerde retouching - €1.100
+            <strong>Premium:</strong> 8 uur, 500+ foto’s, volledige evenement, geavanceerde retouching - €600
           </p>
         </div>
       </div>
@@ -31,15 +31,15 @@ const Pricing = () => {
         </p>
         <div>
           <p>
-            <strong>Basis:</strong> 4 uur, 3–5 minuten video, basisbewerking, 1 locatie - €450–€550 <br/>
+            <strong>Basis:</strong> 4 uur, 2–4 minuten video, basisbewerking, 1 locatie - €250–€350 <br />
             *Inclusief 1 revisie.*
           </p>
           <p>
-            <strong>Standaard:</strong> 8 uur, 5–7 minuten video, geavanceerde bewerking, 2 locaties, teasers - €800–€1.000 <br />
+            <strong>Standaard:</strong> 6 uur, 4–6 minuten video, geavanceerde bewerking, 2 locaties, teasers - €450–€600 <br />
             *Inclusief 2 revisies.*
           </p>
           <p>
-            <strong>Premium:</strong> 10–12 uur, volledige video, dronebeelden, cinematische bewerking - €1.500–€2.000 <br />
+            <strong>Premium:</strong> 8–10 uur, volledige video, dronebeelden, cinematische bewerking - €800–€1.200 <br />
             *Inclusief 2 revisies.*
           </p>
         </div>
@@ -47,19 +47,16 @@ const Pricing = () => {
 
       <h3>Extra Opties & Toevoegingen</h3>
       <div>
-        <p>Dronebeelden: €120-€300</p>
-        <p>Extra uren: €85/uur</p>
-        <p>Snel levering: +20% van de pakketprijs</p>
-        <p>Extra revisies: €75/uur</p>
-        <p>Nachtopnames: +30% toeslag</p>
+        <p>Dronebeelden: €100–€200</p>
+        <p>Extra uren: €60/uur</p>
+        <p>Snel levering: +15% van de pakketprijs</p>
+        <p>Extra revisies: €50/uur</p>
+        <p>Nachtopnames: +20% toeslag</p>
       </div>
 
       <h3>Aangepaste Videopakketten</h3>
       <p>
-        *Voor een offerte op maat, neem contact op via
-        de contactpagina.*
-        {/* *Voor een offerte op maat, neem contact op via
-        het contactformulier.* */}
+        *Voor een offerte op maat, neem contact op via de contactpagina.*
       </p>
       <p>*Alle prijzen exclusief btw*</p>
     </section>
